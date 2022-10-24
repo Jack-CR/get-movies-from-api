10b27bf4ed414315fceab6f1d54af1dd1dcca710
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-10-24 12:27:27 -0600

    Adding styles to the movie list

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ const getMovie = async (movieName) => {
     try {
         let res = await fetch(`https://api.tvmaze.com/search/shows?q=${movieName}`),
             json = await res.json();
-            $loader.style.color="red"
         DomMovies(json);
     } catch (error) {
 
